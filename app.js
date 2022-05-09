a491fbaca7c2650913d5e2b135c8700b7a39be73
Jorge <jjavierdenis@gmail.com>
2022-05-09 17:48:17 -0400

    created auth.js, utils.js in routers folder, register.pug in views folder

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const { restoreUser } = require('./auth')
 
 const app = express();
 
@@ -18,14 +19,16 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(sessionSecret));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // set up session middleware
 const store = new SequelizeStore({ db: sequelize });
 
 app.use(
     session({
+        name: 'filmdium.sid',
         secret: sessionSecret,
         store,
         saveUninitialized: false,
@@ -35,6 +38,8 @@ app.use(
 
 // create Session table if it doesn't already exist
 store.sync();
+
+app.use(restoreUser)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
+const { csrfProtection, asyncHandler } = require('./utils');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('user-register', {
+    title: 'Register',
+    user,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const userValidators = [
+  // TODO Define the user validators.
+];
+
+router.post('/register', csrfProtection, userValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      emailAddress,
+      firstName,
+      lastName,
+      password,
+    } = req.body;
+
+    const user = db.User.build({
+      emailAddress,
+      firstName,
+      lastName,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await user.save();
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('user-register', {
+        title: 'Register',
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
 
 module.exports = router;
