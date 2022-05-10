0ca1887182eb152bd4eea2f6e2b20775c105d30d
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2022-05-10 14:10:27 -0400

    Created seed data files and created stories routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const storiesRouter = require('./routes/stories');
 const { restoreUser } = require('./auth')
 
 const app = express();
@@ -43,6 +44,7 @@ app.use(restoreUser)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/stories', storiesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/stories.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { csrfProtection, asyncHandler } = require('./utils');
+
+const router = express.Router();
+
+
+module.exports = router;
