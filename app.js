d6f1a7570912949f1e51161264eb99835c149697
Jacob <jacobjthompson86@gmail.com>
2022-05-10 17:52:38 -0600

    working on story form and stories/new

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stories', storiesRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
--- routes/stories.js
+++ routes/stories.js
@@ -4,6 +4,16 @@ const db = require('../db/models')
 
 const router = express.Router();
 
+router.get('/new', csrfProtection, asyncHandler( async(req, res) => {
+    console.log("-------------------------------- is this hitting? --------------------------------------")
+    const stories = await db.Story.findAll()
+    console.log(stories, "--------------------------------- hwo abotu now? ------------------------------------")
+    res.render('storyForm', {
+        stories,
+        csrfToken: req.csrfToken(),
+    });
+  }));
+
 
 router.get("/", asyncHandler(async (req, res) => {
     console.log('In get stories /')
@@ -29,10 +39,28 @@ router.get("/:id", asyncHandler(async (req, res) => {
     res.render("story", { story })
 }))
 
+// router.get("/new", csrfProtection, asyncHandler(async (req, res) => {
+//     console.log("----------- before storuy.build------------")
+//     const story = db.Story.build()
+//     console.log("----------- story ------------")
 
-router.post("/", csrfProtection, asyncHandler(async (req, res) => {
+//     res.render("storyForm", {story, csrfToken: req.csrfToken()})
 
-}))
+// }))
+router.use((req, res, next) => {
+    console.log("--------- YOOOOOOOOOOOOOOOO THIS IS A TEST READ THIS MAN PLAEASD WORK ")
+    next()
+})
+
+
+// router.post("/", csrfProtection, asyncHandler(async (req, res) => {
+//     const {title, content, imgUrl, userId} = req.body;
+
+
+//     res.render("storyForm", {})
+
+
+// }))
 
 
 module.exports = router;
