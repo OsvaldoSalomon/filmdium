aca0a7437d6606c5b54765e430156dd2a8d8edd5
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2022-05-10 18:44:31 -0400

    Finished GET all stories and GET story by id

--- db/models/user.js
+++ db/models/user.js
@@ -1,22 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    fullName: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING
-  }, {});
-  User.associate = function (models) {
-    // associations can be defined here
-    const columnMapping = {
-      through: 'Like',
-      otherKey: 'storyId',
-      foreignKey: 'userId'
-    }
-    User.belongsToMany(models.Story, columnMapping);
-    User.hasMany(models.Comment, { foreignKey: 'userId' });
-    User.hasMany(models.Like, { foreignKey: 'userId' });
+    const User = sequelize.define('User', {
+        fullName: DataTypes.STRING,
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        hashedPassword: DataTypes.STRING
+    }, {});
+    User.associate = function (models) {
+        // associations can be defined here
+        const columnMapping = {
+            through: 'Like',
+            otherKey: 'storyId',
+            foreignKey: 'userId',
+            as: 'likes'
+        }
+        User.belongsToMany(models.Story, columnMapping);
+        User.hasMany(models.Story, { foreignKey: 'userId', as: 'author' })
+        User.hasMany(models.Comment, { foreignKey: 'userId' });
+        User.hasMany(models.Like, { foreignKey: 'userId' });
 
-  };
-  return User;
+    };
+    return User;
 };
--- routes/stories.js
+++ routes/stories.js
@@ -6,19 +6,33 @@ const router = express.Router();
 
 
 router.get("/", asyncHandler(async (req, res) => {
-    const stories = await db.Story.findAll();
-    res.render("stories", {stories})
+    console.log('In get stories /')
+    const stories = await db.Story.findAll({
+        include: {
+            model: db.User,
+            as: 'author'
+        }
+    });
+    console.log("--------- Stories -----------", stories[0].author);
+
+    res.render("stories", { stories })
 }));
 
-router.get("/", asyncHandler(async (req, res) => {
-    const story = await db.Story.findByPk()
+router.get("/:id", asyncHandler(async (req, res) => {
+    const story = await db.Story.findByPk(req.params.id, {
+        include: {
+            model: db.User,
+            as: 'author'
+        }
+    })
+    const author = await db.User.findByPk(story.userId);
+    res.render("story", { story })
 }))
 
 
-router.post("/", csrfProtection, asyncHandler(async(req, res) => {
+router.post("/", csrfProtection, asyncHandler(async (req, res) => {
 
 }))
-
 
 
 module.exports = router;
